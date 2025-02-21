a824280842def6ccfcc22905840cb9a1bc02017b
parag lakum <paragl.albiorix@gmail.com>
2025-02-21 15:30:57 +0530

    point value fixed

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function convert(){
     if (tof.checked){
         temp = Number(num.value);
         temp = temp * 9/5 + 32;
-        result.textContent = temp + "°F"
+        result.textContent = temp.toFixed(1) + "°F"
     }
     else if(toc.checked){
         temp = Number(num.value);
         temp = (temp - 32) * (5/9);
-        result.textContent = temp + "°C"
+        result.textContent = temp.toFixed(1) + "°C"
     }else{
         result.textContent = "Select Unit"
     }
